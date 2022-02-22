eb4a351ef0f096c07601587c02ac9645fb4ba67d
MushiTea <94145946+MushiTea@users.noreply.github.com>
2022-02-21 22:53:22 -0600

    testing to see if commit and push works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP.java
@@ -17,7 +17,9 @@ public class TeleOP extends OpMode {
     private double factor;
     private BNO055IMU imu;
     @Override
-    //lmao
+
+    //Testing to commit and push code
+
     public void init() {
         //Maps all the variables to its respective hardware
         leftFront = (DcMotorEx) hardwareMap.dcMotor.get("FL");
